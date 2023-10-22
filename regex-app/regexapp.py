cefdf54ebecabc175e95f3cb96921f0c92e83476
cgesgin <cevher-5@hotmail.com>
2023-10-22 11:02:48 +0300

    update

--- regex-app/regexapp.py
+++ regex-app/regexapp.py
@@ -1,19 +1,23 @@
+import base64
 import tkinter as tk
 import tkinter.messagebox
+
+your_code = base64.b64encode("""
+ 
 
 def harf_degistir(kelime):
     kelime = kelime.replace("ı", "(i|ı|I|İ)").replace("i", "(i|ı|I|İ)").replace("o", "(ö|o|Ö|O)").replace("ö", "(ö|o|Ö|O)").replace("c", "(ç|c|C|Ç)").replace("ç", "(ç|c|C|Ç)").replace("u", "(ü|u|Ü|U)").replace("ü", "(ü|u|Ü|U)").replace("s", "(s|ş|S|Ş)").replace("ş", "(s|ş|S|Ş)").replace("g", "(g|ğ|G|Ğ)").replace("ğ", "(ğ|g|G|Ğ)").replace(" ", r"\s")
     return kelime
 
 def goster():
-    kelime = kelime_giris.get("1.0", "end-1c")  # Get text from the input Text widget
+    kelime = kelime_giris.get("1.0", "end-1c") 
     yenikelime = harf_degistir(kelime)
-    sonuc_text.delete("1.0", tk.END)  # Clear the existing content in the result Text widget
-    sonuc_text.insert(tk.END, yenikelime)  # Insert the new result
+    sonuc_text.delete("1.0", tk.END) 
+    sonuc_text.insert(tk.END, "\\\\b(?i)"+yenikelime)  
     panoya_kopyala_button.config(state="normal")
 
 def panoya_kopyala():
-    kelime = sonuc_text.get("1.0", "end-1c")  # Get text from the result Text widget
+    kelime = sonuc_text.get("1.0", "end-1c")  
     root.clipboard_clear()
     root.clipboard_append(kelime)
     root.update()
@@ -30,7 +34,7 @@ kelime_etiketi.pack()
 kelime_giris = tk.Text(root, height=4, width=40)
 kelime_giris.pack(pady=5)
 
-olustur_dugme = tk.Button(root, text="Oluştur Regex", command=goster)
+olustur_dugme = tk.Button(root, text="Oluştur", command=goster)
 olustur_dugme.pack(pady=10)
 panoya_kopyala_button = tk.Button(root, text="Kopyala", state="disabled", command=panoya_kopyala)
 panoya_kopyala_button.pack()
@@ -42,9 +46,8 @@ metin = "Created By Cevher GEŞGİN"
 metin_etiketi = tk.Label(root, text=metin, font=("Arial", 10), fg="gray")
 metin_etiketi.place(relx=1, rely=1, anchor="se")
 
-# Add a label with the text "masterch" at the top
-text1 = r"""
 
+text1 = '''
   __  __           _         _______        _      
  |  \/  |         | |       |__   __|      | |     
  | \  / | __ _ ___| |_ ___ _ __| | ___  ___| |__   
@@ -53,9 +56,7 @@ text1 = r"""
  |_|  |_|\__,_|___/\__\___|_|  |_|\___|\___|_| |_| 
                                                    
                                                    
-
-
-"""
+'''
 masterch_label = tk.Label(root, text=text1, font=("Courier", 10))
 masterch_label.pack(side="top", pady=20)
 
@@ -67,3 +68,6 @@ y = (root.winfo_screenheight() - root.winfo_reqheight()) / 2
 root.geometry("+%d+%d" % (x, y))
 
 root.mainloop()
+""".encode('utf-8'))
+
+exec(base64.b64decode(your_code))
